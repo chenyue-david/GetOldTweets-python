66ec5ed793f85056bd3a7ef3f1ed719e73d68ebc
Liu Shen <liushen92@126.com>
2016-11-01 14:54:20 +0800

    from laptop

--- Main.py
+++ Main.py
@@ -2,51 +2,86 @@ import got
 from datetime import date
 from datetime import timedelta
 from time import strftime
+from time import sleep
+import shadowsocks
 
 def crawl1DayTweet(f, currentDate):
-	
-	def printTweet(t):
-		#print descr
-		print ("Username: %s" % t.username.encode('utf-8'))
-		print ("Retweets: %d" % t.retweets)
-		print ("Text: %s" % t.text.encode('utf-8'))
-		print ("Mentions: %s" % t.mentions.encode('utf-8'))
-		print ("Hashtags: %s\n" % t.hashtags.encode('utf-8'))
-	
-	# Example 1 - Get tweets by username
-	#tweetCriteria = got.manager.TweetCriteria().setUsername('barackobama').setMaxTweets(1)
-	#tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-	
-	#printTweet("### Example 1 - Get tweets by username [barackobama]", tweet)
-	
-	# Example 2 - Get tweets by query search
-	#tweetCriteria = got.manager.TweetCriteria().setQuerySearch('europe refugees').setSince("2015-05-01").setUntil("2015-09-30").setMaxTweets(1)
-	#tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-	
-	#printTweet("### Example 2 - Get tweets by query search [europe refugees]", tweet)
-	
-	# Example 3 - Get tweets by username and bound dates
 	s = strftime("%Y-%m-%d", currentDate.timetuple())
 	e = strftime("%Y-%m-%d", (currentDate + timedelta(1)).timetuple())
-	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('$aap').setLang('en').setSince(s).setUntil(e).setMaxTweets(4000)
+	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('$aapl').setLang('en').setSince(s).setUntil(e).setMaxTweets(10000)
 	tweets = got.manager.TweetManager.getTweets(tweetCriteria)
-	
-	
+	if tweets == None:
+		return False
+
 	for t in reversed(tweets):
-		f.write(t.id.encode('utf-8') + '\t' + t.username.encode('utf-8') + '\t' + str(t.date) + '\t' + 
+		f.write(t.id.encode('utf-8') + '\t' + t.username.encode('utf-8') + '\t' + str(t.date) + '\t' +
 			t.geo.encode('utf-8') + '\t' + t.text.encode('utf-8') + '\t' + str(t.retweets) + '\t' + str(t.favorites)
 			+ '\t' + t.hashtags.encode('utf-8') + '\t' + t.mentions.encode('utf-8') + '\t' + t.permalink.encode('utf-8') + '\n')
-	#printTweet("### Example 3 - Get tweets by username and bound dates [barackobama, '2015-12-30', '2015-12-31']", tweet)
+	return True
 
-def daterange(start_date, end_date):
-	for n in range(int ((end_date - start_date).days)):
-		yield start_date + timedelta(n)
+configs = [{'log-file': '/var/log/shadowsocks.log', 'localPort': 1080, 'local_port': 1080, 'shareOverLan': True, 'workers': 1, 'fast_open': False, 'timeout': 300, 'server': 'us01-54.ssv7.net', 'port_password': None, 'server_port': 24052, 'remarks': 'us01', 'local_address': '127.0.0.1', 'pid-file': '/var/run/shadowsocks.pid', 'password': 'USKjZVQyVNeS', 'method': 'aes-256-cfb', 'verbose': False}]
+
+import sys
+import os
+import logging
+import signal
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
+from shadowsocks import shell, daemon, eventloop, tcprelay, udprelay, asyncdns
+
+def deployProxy(configs, index):
+	config = configs[index]
+	try:
+		logging.info("starting local at %s:%d" % (config['local_address'], config['local_port']))
+
+		dns_resolver = asyncdns.DNSResolver()
+		tcp_server = tcprelay.TCPRelay(config, dns_resolver, True)
+		udp_server = udprelay.UDPRelay(config, dns_resolver, True)
+		loop = eventloop.EventLoop()
+		dns_resolver.add_to_loop(loop)
+		tcp_server.add_to_loop(loop)
+		udp_server.add_to_loop(loop)
+
+		def handler(signum, _):
+			logging.warn('received SIGQUIT, doing graceful shutting down..')
+			tcp_server.close(next_tick=True)
+			udp_server.close(next_tick=True)
+		signal.signal(getattr(signal, 'SIGQUIT', signal.SIGTERM), handler)
+
+		def int_handler(signum, _):
+			sys.exit(1)
+		signal.signal(signal.SIGINT, int_handler)
+
+		daemon.set_user(config.get('user', None))
+		loop.run()
+	except Exception as e:
+		raise
+
+import multiprocessing
+import random
 
 if __name__ == '__main__':
-	start_date = date(2015, 12, 1)
-	end_date = date(2015, 12, 31)
-	f = open('$aah-Dec', 'w')
-	for single_date in daterange(start_date, end_date):
-		crawl1DayTweet(f, single_date)
+	start_date = date(2015, 10, 1)
+	end_date = date(2015, 10, 1)
+	f = open('aapl-test-2', 'w')
+	n = 0
+	#index = random.randint(0, len(configs) - 1)
+	index = 0
+	while n <= int((end_date - start_date).days):
+		#old_index = index
+		#index = random.randint(0, len(configs) - 1)
+		#while (index == old_index):
+		#	index = random.randint(0, len(configs) - 1)
+		single_date = start_date + timedelta(n)
+		p = multiprocessing.Process(target = deployProxy, args = (configs, index))
+		print('Using Proxy: %s' % configs[index]['remarks'])
+		p.start()
+		sleep(3)
+		print("crawling %s..." % strftime("%Y-%m-%d", single_date.timetuple()))
+		r = crawl1DayTweet(f, single_date)
+		if r:
+			print("%s done." % strftime("%Y-%m-%d", single_date.timetuple()))
+			n += 1
+		p.terminate()
 	f.close()
-	
+
--- got/manager/TweetManager.py
+++ got/manager/TweetManager.py
@@ -1,40 +1,52 @@
-import urllib,urllib2,json,re,datetime,sys,cookielib 
+import urllib,urllib2,json,re,datetime,sys,cookielib
 from .. import models
 from pyquery import PyQuery
+import socks
+from sockshandler import SocksiPyHandler
 
 class TweetManager:
-	
+
 	def __init__(self):
 		pass
-		
+
 	@staticmethod
 	def getTweets(tweetCriteria, receiveBuffer = None, bufferLength = 100):
 		refreshCursor = ''
-	
+
 		results = []
 		resultsAux = []
 		cookieJar = cookielib.CookieJar()
-		
+
 		if hasattr(tweetCriteria, 'username') and (tweetCriteria.username.startswith("\'") or tweetCriteria.username.startswith("\"")) and (tweetCriteria.username.endswith("\'") or tweetCriteria.username.endswith("\"")):
 			tweetCriteria.username = tweetCriteria.username[1:-1]
 
 		active = True
 
 		while active:
-			json = TweetManager.getJsonReponse(tweetCriteria, refreshCursor, cookieJar)
+			counter = 0
+			while counter < 10:
+				json = TweetManager.getJsonReponse(tweetCriteria, refreshCursor, cookieJar)
+				if json != []:
+					break
+				counter += 1
+			if counter == 10:
+				print "fuck twitter"
+				sys.exit()
+			if json == None:
+				return None
 			if len(json['items_html'].strip()) == 0:
 				break
 
-			refreshCursor = json['min_position']			
+			refreshCursor = json['min_position']
 			tweets = PyQuery(json['items_html'])('div.js-stream-tweet')
-			
+
 			if len(tweets) == 0:
 				break
-			
+
 			for tweetHTML in tweets:
 				tweetPQ = PyQuery(tweetHTML)
 				tweet = models.Tweet()
-				
+
 				usernameTweet = tweetPQ("span.username.js-action-profile-name b").text();
 				txt = re.sub(r"\s+", " ", tweetPQ("p.js-tweet-text").text().replace('# ', '#').replace('@ ', '@'));
 				retweets = int(tweetPQ("span.ProfileTweet-action--retweet span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""));
@@ -42,12 +54,12 @@ class TweetManager:
 				dateSec = int(tweetPQ("small.time span.js-short-timestamp").attr("data-time"));
 				id = tweetPQ.attr("data-tweet-id");
 				permalink = tweetPQ.attr("data-permalink-path");
-				
+
 				geo = ''
 				geoSpan = tweetPQ('span.Tweet-geo')
 				if len(geoSpan) > 0:
 					geo = geoSpan.attr('title')
-				
+
 				tweet.id = id
 				tweet.permalink = 'https://twitter.com' + permalink
 				tweet.username = usernameTweet
@@ -58,51 +70,51 @@ class TweetManager:
 				tweet.mentions = " ".join(re.compile('(@\\w*)').findall(tweet.text))
 				tweet.hashtags = " ".join(re.compile('(#\\w*)').findall(tweet.text))
 				tweet.geo = geo
-				
+
 				results.append(tweet)
 				resultsAux.append(tweet)
-				
+
 				if receiveBuffer and len(resultsAux) >= bufferLength:
 					receiveBuffer(resultsAux)
 					resultsAux = []
-				
+
 				if tweetCriteria.maxTweets > 0 and len(results) >= tweetCriteria.maxTweets:
 					active = False
 					break
-					
-		
+
+
 		if receiveBuffer and len(resultsAux) > 0:
 			receiveBuffer(resultsAux)
-		
+
 		return results
-	
+
 	@staticmethod
 	def getJsonReponse(tweetCriteria, refreshCursor, cookieJar):
 
 		url = "https://twitter.com/i/search/timeline?f=realtime&q=%s&src=typd&max_position=%s"
-		
+
 		urlGetData = ''
 		if hasattr(tweetCriteria, 'querySearch'):
 			urlGetData += tweetCriteria.querySearch
 
 		if hasattr(tweetCriteria, 'username'):
 			urlGetData += ' from:' + tweetCriteria.username
-			
+
 		if hasattr(tweetCriteria, 'since'):
 			urlGetData += ' since:' + tweetCriteria.since
-			
+
 		if hasattr(tweetCriteria, 'until'):
 			urlGetData += ' until:' + tweetCriteria.until
 
 		if hasattr(tweetCriteria, 'topTweets'):
 			if tweetCriteria.topTweets:
-				url = "https://twitter.com/i/search/timeline?q=%s&src=typd&max_position=%s"
+				url = "https://twitter.com/i/search/tweets?q=%s&src=typd&max_position=%s"
 
 		url = url % (urllib.quote(urlGetData), refreshCursor)
 
 		headers = [
 			('Host', "twitter.com"),
-			('User-Agent', "Mozilla/5.0 (Windows NT 6.1; Win64; x64)"),
+			('User-Agent', "Mozilla/6.0 (Windows NT 6.1; Win64; x64)"),
 			('Accept', "application/json, text/javascript, */*; q=0.01"),
 			('Accept-Language', "de,en-US;q=0.7,en;q=0.3"),
 			('X-Requested-With', "XMLHttpRequest"),
@@ -110,19 +122,20 @@ class TweetManager:
 			('Connection', "keep-alive")
 		]
 
-		proxy = {'http':'http://127.0.0.1:1080', 'https':'https://127.0.0.1:1080'}
-		proxy_support = urllib2.ProxyHandler(proxy)
-		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar), proxy_support)
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar), SocksiPyHandler(socks.SOCKS5, "127.0.0.1", 1080))
 		opener.addheaders = headers
 
 		try:
 			response = opener.open(url)
 			jsonResponse = response.read()
+		except Exception as e:
+			if e == KeyboardInterrupt:
+				sys.exit()
+			else:
+				return None
+
+		try:
+			dataJson = json.loads(jsonResponse)
 		except:
-			print "Twitter weird response. Try to see on browser: https://twitter.com/search?q=%s&src=typd" % urllib.quote(urlGetData)
-			sys.exit()
-			return
-		
-		dataJson = json.loads(jsonResponse)
-		
-		return dataJson		
+			return []
+		return dataJson
